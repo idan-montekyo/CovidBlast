c28a9428e42dd632f54a81d71d6f18830f4d725b
idan-montekyo <idanmontekyo@gmail.com>
2022-01-03 19:56:33 +0200

    Finished upgrades_dialog.xml pattern.

--- app/src/main/java/com/example/covidblast/Syringe.java
+++ app/src/main/java/com/example/covidblast/Syringe.java
@@ -1,4 +1,15 @@
 package com.example.covidblast;
 
+import android.graphics.Bitmap;
+
 public class Syringe {
+
+    private int height, width, x;
+    final private int y = 300;
+    private int attack, attack_speed;
+    Bitmap syringe;
+
+    public Syringe() {
+
+    }
 }
